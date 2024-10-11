932343045735388edc2daf6b7c628196c3c2d968
MicheleFumi <michele.fumi96@gmail.com>
2024-10-11 14:41:55 +0200

    add first solution

---
+++ main.js
@@ -0,0 +1,73 @@
+/* Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore. */
+
+const bici = [
+    { 
+        nome: 'Atala', peso: 7 
+
+    },
+
+    { 
+        nome: 'Bottecchia', peso: 9 
+
+    },
+
+    {
+         nome: 'Graziella', peso: 6 
+     },
+
+    {
+        nome: 'Graziella', peso: 10 
+    }
+];
+   console.log(bici[0]);
+     
+
+
+
+/* Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+
+const squadre = [
+    { 
+        nome: 'Milan',  punti_fatti: 0 , falli_subiti: 0 ,
+
+    },
+
+    { 
+        nome: 'Inter',  punti_fatti: 0 , falli_subiti: 0 ,
+
+    },
+
+    { 
+
+        nome: 'Juve',  punti_fatti: 0 , falli_subiti: 0 , 
+        
+     },
+
+
+    {
+
+        nome: 'Roma',  punti_fatti: 0 , falli_subiti: 0 , 
+
+    }
+];
+
+const punti_fatti= Math.random() * 10
+const falli_subiti= Math.random() * 10
+for (let i = 0; i < squadre.length; i++) {
+    const falli_random = squadre[0] ;
+    console.log(falli_random);
+}
+
+
+/* Snack 3 (Bonus)
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. */
